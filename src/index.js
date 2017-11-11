9af10d41dea974825563ec0a7a4a6182e3336b77
Ariel Herman <aeh.herman@gmail.com>
2017-11-11 14:41:31 -0500

    can view notes from server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* src/index.js */
+$(document).ready(() => {
+  fetch("http://localhost:3000/notes")
+    .then(res => res.json())
+    .then(json => {
+      json.forEach(note => {
+        $("#notes-list").append(new Note(note).renderListItem());
+      });
+    });
+});
